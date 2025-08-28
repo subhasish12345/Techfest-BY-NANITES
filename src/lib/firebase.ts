b9958632d4e41bea507c16425b5f7719cf44e6f6
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-08-28 04:13:34 +0000

    FirebaseError: Firebase: Error (auth/api-key-not-valid.-please-pass-a-va

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,6 +5,7 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+require('dotenv').config()
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
