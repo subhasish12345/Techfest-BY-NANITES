d6fc2ad10b869fe890f0a30f5567c197f62bd9dc
Subhasish Nayak <subhasishnayak38@gmail.com>
2025-09-01 20:31:10 +0000

    for the deploy on the render we have to clear all the errors from the co

--- src/app/admin/events/edit/[id]/page.tsx
+++ src/app/admin/events/edit/[id]/page.tsx
@@ -1,10 +1,8 @@
 
 "use client"
-import { EventForm } from "../../event-form";
+import { EventForm } from "../event-form";
 
 export default function EditEventPage({ params }: { params: { id: string } }) {
     // This form will now pre-populate from dummy data
     return <EventForm eventId={params.id} />;
 }
-
-    
--- src/app/admin/events/event-form.tsx
+++ src/app/admin/events/event-form.tsx
@@ -12,7 +12,7 @@ import { events as dummyEvents } from "@/lib/data";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
